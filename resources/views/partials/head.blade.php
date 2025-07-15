ae53bae2d3b76683d3eabb71a0ee25c39f42db22
Jostin <jostin@example.com>
2025-07-14 20:28:31 -0600

    Espanol login y favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,8 +3,7 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
-<link rel="icon" href="/TiquisateNewsLogo.png" type="image/png">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
+<link rel="icon" type="image/png" href="{{ asset('TiquisateNewsLogo.png') }}">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
